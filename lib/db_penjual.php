053fcb516c03e7bce8b88e4cd0e300a6e3a243da
muhammadrais <muhammad8163@students.amikom.ac.id>
2016-12-13 17:02:25 +0700

    dari rais

--- lib/db_penjual.php
+++ lib/db_penjual.php
@@ -11,7 +11,7 @@
 
     public function readAllPenjualan()
     {
-      $query = "Select * from detail_penjualan";
+      $query = "SELECT dp.no_penjualan, pk.merk as pupuk, p.nama as pembeli, dp.jumlah, dp.harga_jual FROM detail_penjualan dp JOIN pupuk pk ON dp.id_pupuk = pk.id_pupuk JOIN penjualan pj ON pj.id_penjualan = dp.id_penjualan JOIN pembeli p ON p.id_pembeli = pj.id_pembeli";
       return $this->db->getRows($query);
     }
   }
